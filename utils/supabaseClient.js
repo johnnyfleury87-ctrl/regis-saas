7f7ca6b7356118361f4c0c67ea90d2f6e95425cb
johnnyfleury87-ctrl <johnny.fleury87@gmail.com>
2025-11-28 09:06:12 +0000

    Fix SUPABASE_SERVICE_KEY env var

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,7 +1,6 @@
-// Import depuis CDN pour compatibilité navigateur
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://mkcjwhkcqfjgaoqjbmsy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rY2p3aGtjcWZqZ2Fxb2pibXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNDg4ODMsImV4cCI6MjA3OTYyNDg4M30.XANf4CpYvG8U1S_9OGuIPPK_NkvfmwrT0XHfVTpOIdY";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY   // <-- LA SEULE BONNE CLÉ
+);
