d92f1722c1d8ebe187626efbcb0477c3874d39b3
johnnyfleury87-ctrl <johnny.fleury87@gmail.com>
2025-11-29 13:59:24 +0000

    Gestion locataires régie

--- api/regie/locataires.js
+++ api/regie/locataires.js
@@ -1,4 +1,5 @@
-import { supabase } from "../supabase.js";
+import { supabaseServer as supabase } from "../../utils/supabaseClient.js";
+
 
 export default async function handler(req, res) {
   const method = req.method;
