638395a7e7e59b5548dea863bef6a66a2c793120
johnnyfleury87-ctrl <johnny.fleury87@gmail.com>
2025-12-05 04:49:19 +0000

    fix: vue entreprise accepte missions

--- api/_handlers/updateMissionStatusHandler.js
+++ api/_handlers/updateMissionStatusHandler.js
@@ -1,5 +1,5 @@
 // Importe le client Supabase partagé
-import { supabaseServer } from "../../utils/supabaseClient.js";
+import { supabaseServer as supabase } from "../../utils/supabaseClient.js";
 
 // Exporte la fonction handler asynchrone
 module.exports = async (req, res) => {
