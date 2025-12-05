70553b955de71902df8fdfd0cb0742c64173fce4
johnnyfleury87-ctrl <johnny.fleury87@gmail.com>
2025-12-05 04:58:05 +0000

    fix: vue entreprise accepte missions

--- api/_handlers/createTicketHandler.js
+++ api/_handlers/createTicketHandler.js
@@ -1,4 +1,4 @@
-import { supabaseServer as supabase } from "../../utils/supabaseClient.js";
+import { supabaseServer } from "../../utils/supabaseClient.js";
 
 
 export default async function handler(req, res) {
